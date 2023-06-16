830fcba78d47b01b0248956a0eab2467f539667e
justin-qf <justin1998.qf@gmail.com>
2023-06-16 19:41:25 +0530

    - Init APis Calls - DONE - First Final Apk Provide to Amar 16-06-2023 FROM MJ

---
+++ app/src/main/java/com/app/smwc/Activity/OtpActivity/SendOtpResponse.kt
@@ -0,0 +1,9 @@
+package com.app.omcsalesapp.Activity.SignInActivity
+
+import com.google.gson.annotations.SerializedName
+
+data class SendOtpResponse(
+    @SerializedName("status") var status: Int? = null,
+    @SerializedName("message") var message: String? = null,
+    @SerializedName("otp") var otp: String? = null
+)
